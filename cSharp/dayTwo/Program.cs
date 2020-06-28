3f89b72f99eeffd1dd3df2aac560daf1fff7f603
Shawn <shawn.harris3510@gmail.com>
2020-06-27 22:27:42 -0400

    product of indices populating correctly

---
+++ cSharp/dayTwo/Program.cs
@@ -0,0 +1,41 @@
+﻿// Given an array of integers, return a new array such that
+// each element at index i of the new array is the product of all the
+// numbers in the original array except the one at i.
+
+// For example, if our input was [1, 2, 3, 4, 5],
+// the expected output would be [120, 60, 40, 30, 24].
+// If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+using System;
+
+namespace dayTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            allButI();
+        }
+        static void allButI()
+        {
+            // declare array
+            int[] arrOne = {1, 2, 3, 4, 5};
+            int indicesProduct = 1;
+            // loop through array to find product of indices
+            for (int i = 0; i < arrOne.Length; i++)
+            {
+                // find total indices product
+                indicesProduct = indicesProduct * arrOne[i];
+                // Console.WriteLine(indicesProduct);
+            }
+            Console.WriteLine(indicesProduct);
+            // loop through array
+            for (int i = 0; i < arrOne.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
+//  
