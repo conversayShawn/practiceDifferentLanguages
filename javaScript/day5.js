6fc9b615217b3a962418da63debd3818715649f6
Shawn <shawn.harris3510@gmail.com>
2020-07-02 01:42:42 -0400

    matching parenthesis

---
+++ javaScript/day5.js
@@ -0,0 +1,10 @@
+// A classic problem — Check for balanced parentheses in an expression. 
+// Two brackets are considered to be a matched pair if the an opening bracket 
+// (i.e., (, [, or { ) occurs to the left of a closing bracket (i.e., ), ], or }) 
+// of the exact same type. There are three types of matched pairs of brackets: 
+// [], {}, and ().
+
+// Write a balancedParenthesis function that takes a string as input and 
+// returns a boolean — if the parentheses in the input string are ‘balanced’, 
+// then return true, else return false. For example, the program should 
+// print true for exp = “[()]{}{[()()]()}” and false for exp = “[(])”
