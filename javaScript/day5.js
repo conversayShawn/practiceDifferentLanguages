5b44b91a923afa764262a929b11a1d9125eb97d0
Shawn <shawn.harris3510@gmail.com>
2020-07-03 02:36:22 -0400

    counts equal, but pairs not matched

--- javaScript/day5.js
+++ javaScript/day5.js
@@ -1,17 +1,17 @@
-// A classic problem — Check for balanced parentheses in an expression. 
-// Two brackets are considered to be a matched pair if the an opening bracket 
-// (i.e., (, [, or { ) occurs to the left of a closing bracket (i.e., ), ], or }) 
-// of the exact same type. There are three types of matched pairs of brackets: 
+// A classic problem — Check for balanced parentheses in an expression.
+// Two brackets are considered to be a matched pair if the an opening bracket
+// (i.e., (, [, or { ) occurs to the left of a closing bracket (i.e., ), ], or })
+// of the exact same type. There are three types of matched pairs of brackets:
 // [], {}, and ().
 
-// Write a balancedParenthesis function that takes a string as input and 
-// returns a boolean — if the parentheses in the input string are ‘balanced’, 
-// then return true, else return false. For example, the program should 
+// Write a balancedParenthesis function that takes a string as input and
+// returns a boolean — if the parentheses in the input string are ‘balanced’,
+// then return true, else return false. For example, the program should
 // print true for exp = “[()]{}{[()()]()}” and false for exp = “[(])”
 
-console.log("hello")
+console.log('hello');
 // declare variables
-let exp = "(())()((()())())"
+let exp = '(())()((()())())';
 // parenthesis
 let openP = 0;
 let closeP = 0;
@@ -22,29 +22,36 @@ let closeP = 0;
 // let openC = 0;
 // let closeC = 0;
 
-const balancedParenthesis = () => 
-{
-    //split string
-    let individualParenthsis = exp.split("");
-    // console.log(individualParenthsis);
-    // loop through individualParenthesis
-    for (let i = 0; i < individualParenthsis.length; i++) {
-        // if "(" or ")"
-        if (individualParenthsis[i] === "(") 
-        {
-            //increment open (
-            openP++;
-        } 
-        if (individualParenthsis[i] === ")") 
-        {
-            // decrement close )
-            closeP++;
-        }
-    }
-    console.log(openP);
-    console.log(closeP);
-    // if ( === ), return true
-    // ) !> (, return false
-    // repeat for [] and {}
-}
+// count ()
+const paranthesisCount = () => {
+	//split string
+	let individualParenthsis = exp.split('');
+	// console.log(individualParenthsis);
+	// loop through individualParenthesis
+	for (let i = 0; i < individualParenthsis.length; i++) {
+		// if "(" or ")"
+		if (individualParenthsis[i] === '(') {
+			//increment open (
+			openP++;
+		}
+		if (individualParenthsis[i] === ')') {
+			// decrement close )
+			closeP++;
+		}
+	}
+	console.log(openP);
+	console.log(closeP);
+};
+
+const balancedParenthesis = () => {
+	paranthesisCount();
+	// if ( === ), return true
+	if (openP === closeP) {
+		console.log('true');
+	} else {
+		console.log('false');
+	}
+	// ) !> (, return false
+	// repeat for [] and {}
+};
 balancedParenthesis();
